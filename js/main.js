f54bddebb61847c88872cf34767087783fce8263
shubham103 <shbhmtwr19@gmail.com>
2023-01-04 21:47:02 +0530

    updated conent

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["SHUBHAM KUMAR TIWARI", "DevOps Engg.", "Automation Engg.",  "Go Developer", "Photographer", "Explorer"],
       typeSpeed: 100,
       loop: true,
     });
